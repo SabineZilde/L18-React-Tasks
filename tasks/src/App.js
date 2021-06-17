b365c2ece482b5a996a241b6efb8ab5b241d4845
SabineZilde <sabine.zilde@protonmail.ch>
2021-06-17 21:25:00 +0300

    Task 1

---
+++ tasks/src/App.js
@@ -0,0 +1,11 @@
+import ChangeColor from "./components/ChangeColor";
+
+function App() {
+  return (
+    <div>
+      <ChangeColor />
+    </div>
+  );
+}
+
+export default App;
---
+++ tasks/src/components/ChangeColor.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function ChangeColor() {
+
+    const [text, setText] = useState('Foo');
+    const [color, setColor] = useState('purple');
+
+    const changeText = () => {
+        if (text === 'Foo') {
+            setText('Bar');
+            setColor('brown');
+        } else {
+            setText('Foo');
+            setColor('purple');
+        }
+    }
+
+    return (
+        <div>
+            <h1>Task 1</h1>
+            <p>Click on the button and change the text and color</p>
+            <h2 style={{color: color}}>{text}</h2>
+            <button onClick={changeText}>Click me</button>
+        </div>
+    )
+}
+
+export default ChangeColor;
